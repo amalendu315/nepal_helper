761ce4f55eb5225a2d8ff91a844ad6bb53f1d7ab
Amalendu Pandey <amalendupandey56@gmail.com>
2025-05-07 13:48:22 +0530

    new update done

--- app/api/sale/route.ts
+++ app/api/sale/route.ts
@@ -29,7 +29,7 @@ export async function POST(request: Request) {
         },
       }
     );
-
+    console.log(saleCloudAuthToken);
     return NextResponse.json(response?.data);
   } catch (error) {
     console.error("Error submitting data:", error);
